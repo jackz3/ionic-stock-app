6c326156cca0b871f4dde39bac8ad45e0179ab1b
unknown <jackz3@163.com>
2020-02-03 20:52:26 +0800

    menu add favor stocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router'
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Component,ViewChild } from '@angular/core'
-import { Router } from '@angular/router'
+import { Component } from '@angular/core'
 import {LocalData} from './providers/local-data'
 import {StockService} from './providers/stock'
 import {Config} from './providers/config'
 import {HomePage} from './pages/home/home'
-import {AboutPage} from './pages/about/about'
 import {DetailsPage} from './pages/details/details'
 import {WelcomePage} from './pages/welcome/welcome'
 import { switchMap } from 'rxjs/operators'
@@ -19,7 +18,7 @@ import { switchMap } from 'rxjs/operators'
 })
 export class AppComponent {
   // @ViewChild('', {static: false}) nav: Nav
-  stocks:string[]=[]
+  stocks: any[] = []
   root:any
 	pages:any[]=[
     { title: '自选股', component: HomePage, icon: 'star',type:'favors',name:'HomePage',index:0},
@@ -33,7 +32,7 @@ export class AppComponent {
     private localData:LocalData,
     private stockService:StockService,
     private config:Config,
-    private router: Router
+    private router: Router,
   ) {
     this.localData
         .getFavors().pipe(
@@ -65,28 +64,9 @@ export class AppComponent {
     this.router.navigate([page.url, page.params || {}])
     // this.nav.setRoot(page.component, {tabIndex: page.index,type:page.type})
   }
-  // isActive(page:any) {
-  //   const active=this.nav.getActive()
-  //   if (active){
-  //     if(active.name==='HomePage'){
-  //       if(active.data.type==='boards'){
-  //         if(page.name==='BoardsPage'){
-  //           return 'primary'
-  //         }
-  //       }else if(page.name==='HomePage'){
-  //         return 'primary'
-  //       }
-  //     }else if(active.name===page.name) {
-  //       return 'primary'
-  //     }
-  //   }
-  // }
-  // isDetailsActive(page:any){
-  //   const active=this.nav.getActive()
-  //   if(active.name===page.name && active.data.code===page.code){
-  //     return 'pin'
-  //   }
-  // }
+  isActive (code: string) {
+    return this.router.url.indexOf(`/details/${code}`) === 0
+  }
 
   initializeApp() {
     this.platform.ready().then(() => {
