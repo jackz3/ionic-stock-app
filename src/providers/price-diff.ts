2360f736c1be767296b1ae95d077f0f823d037fe
jack <jackz3@163.com>
2020-02-08 15:46:58 +0800

    before merge

--- src/providers/price-diff.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core'
-
-@Pipe({name: 'priceDiff'})
-export class PriceDiff implements PipeTransform{
-	transform(value) {
-		if(isNaN(value)) return '';
-		return (value>0?'+':'')+value.toFixed(2);
-	}
-}
